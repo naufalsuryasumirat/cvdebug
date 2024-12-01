9fffa1e218afde32e07ad4ca7a77475b66f9361c
Naufal Suryasumirat <naufal.suryasumirat@gmail.com>
2024-12-01 12:57:19 +0900

    chore: adjust tb_params to use move semantics

--- src/params/tb_params.cpp
+++ src/params/tb_params.cpp
@@ -1,15 +1,13 @@
+#include <iostream>
+#include <deque>
+
 #include "tb_params.h"
 
-tb::_pg tb::_pg::operator, (p& b)
+tb::pg tb::pg::operator, (const p& b)
 {
-    _pg group(*this);
+    pg group(*this);
     group.children.push_back(b);
     return group;
-}
-
-tb::p::p()
-    : name("placeholder")
-{
 }
 
 tb::p::p(const char* n)
@@ -24,14 +22,75 @@ tb::p::p(const char* n, dt d)
     this->has_data = true;
 }
 
-tb::p::p(const char* n, const struct p& other)
+tb::p::p(const char* n, struct p&& other)
     : name(n)
     , children({other})
 {
 }
 
-tb::p::p(const char* n, const struct _pg& group)
+tb::p::p(const char* n, struct pg&& group)
     : name(n)
     , children(group.children)
 {
+}
+    
+tb::pg tb::p::operator ,(const p& b)
+{
+    pg group;
+    group.children.push_back(*this);
+    group.children.push_back(b);
+    return group;
+}
+
+tb::p& tb::p::operator =(const p& b)
+{
+    this->name = b.name;
+    this->children = b.children;
+    this->has_data = b.has_data;
+    this->d = b.d;
+    return *this;
+}
+
+void tbd::bfsp(const tb::p& params)
+{
+    std::deque<std::pair<int, const tb::p*>> d = {{0,&params}};
+    while (d.size()) {
+        auto& [depth, param] = d.front();
+        std::cout << std::string(depth, ' ') << param->name << std::endl;
+        for (const auto& c : param->children)
+            d.push_back({depth+1,&c});
+        d.pop_front();
+    }
+}
+
+void tbd::dfsp(const tb::p& params, const uint16_t depth)
+{
+    std::cout << std::string(depth, ' ') << params.name << std::endl;
+    for (size_t i = 0; i < params.children.size(); ++i) {
+        dfsp(params.children[i], depth+1);
+    }
+}
+
+void tbd::dfsp_d(const tb::p& params, const uint16_t depth)
+{
+    fmt::println("d[{:02}]: {}, child: {}", depth, params.name, params.children.size());
+    if (params.has_data) {
+        std::visit([](auto&& v) {
+            using T = std::decay_t<decltype(v)>;
+            if constexpr (std::is_same_v<T, int*>) {
+                fmt::println("\t-> int: {}", *v);
+            } else if constexpr (std::is_same_v<T, float*>) {
+                fmt::println("\t-> float: {:.2f}", *v);
+            } else if constexpr (std::is_same_v<T, double*>) {
+                fmt::println("\t-> double: {:.3f}", *v);
+            } else if constexpr (std::is_same_v<T, bool*>) {
+                fmt::println("\t-> bool: {}", *v);
+            } else { fmt::println("\t-> unknown"); }
+        }, params.d.v);
+    }
+
+    for (size_t i = 0; i < params.children.size(); ++i) {
+        // dfsp_d(*params.children[i], depth+1);
+        dfsp_d(params.children[i], depth+1);
+    }
 }
--- src/params/tb_params.h
+++ src/params/tb_params.h
@@ -2,7 +2,8 @@
 #define TB_PARAMS_H
 
 #include <any>
-// #include <memory>
+#include <cstdint>
+#include <fmt/core.h>
 #include <string>
 #include <variant>
 #include <vector>
@@ -13,15 +14,16 @@ struct p;
 typedef struct p p;
 
 struct dt {
-    std::variant<int*, float*, double*> v;
+    std::variant<int*, float*, double*, bool*> v;
     int max,
         min = 0;
     void (*cb)(std::variant<std::any>*) = NULL;
 };
 
-struct _pg {
+struct pg {
     std::vector<p> children;
-    _pg operator ,(p& b);
+
+    pg operator ,(const p& b);
 };
 
 struct p {
@@ -30,15 +32,22 @@ struct p {
     bool has_data = false;
     dt d;
 
-    p();
     p(const char* n);
     p(const char* n, dt d);
-    p(const char* n, const struct p& other);
-    p(const char* n, const struct _pg& group);
-
-    _pg operator ,(const p& b);
+    p(const char* n, struct p&& other);
+    p(const char* n, struct pg&& group);
+    
+    pg operator ,(const p& b);
     p& operator =(const p& b);
 };
+
+}
+
+namespace tbd {
+
+void bfsp(const tb::p& params);
+void dfsp(const tb::p& params, const uint16_t depth = 0);
+void dfsp_d(const tb::p& params, const uint16_t depth = 0);
 
 }
 
